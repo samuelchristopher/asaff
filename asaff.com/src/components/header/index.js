c66df557ad9e17104fbc16712f40afa375259a32
Samuel Paul Christopher <thesamchris@gmail.com>
2019-06-03 11:19:36 +0800

    smoother animation

--- asaff.com/src/components/header/index.js
+++ asaff.com/src/components/header/index.js
@@ -8,13 +8,12 @@ const Header = () => (
 		<div class={style.asaffLogo}></div>
 		<div class={style.tagline__container}>
 			<p class={style.tagline__static}>We can help you with</p>
-			<div class={style.text__container}>
-				<TextLoop mask={true} interval={2000} springConfig={{ stiffness: 90, damping: 20 }}>
-					<h1 class={style.tagline__dynamic}>ICT</h1>
-					<h1 class={style.tagline__dynamic}>Shipping</h1>
-					<h1 class={style.tagline__dynamic}>Freight Forwarding</h1>
-				</TextLoop>
-			</div>
+			{" "}
+			<TextLoop mask={true} interval={2000} springConfig={{ stiffness: 120, damping: 15 }}>
+				<h1 class={style.tagline__dynamic}>ICT</h1>
+				<h1 class={style.tagline__dynamic}>Shipping</h1>
+				<h1 class={style.tagline__dynamic}>Freight Forwarding</h1>
+			</TextLoop>
 		</div>
 	</header>
 );
