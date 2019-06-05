5b9f677a41e91ca98ace02cfeb04c65c39f369ac
Samuel Paul Christopher <thesamchris@gmail.com>
2019-06-05 11:03:39 +0800

    updated telephone number

--- asaff.com/src/components/contactSection/index.js
+++ asaff.com/src/components/contactSection/index.js
@@ -5,7 +5,7 @@ const ContactSection = () => (
     <section class={style.contact__section}>
         <h5 class={style.section__title}>Contact Us</h5>
         <p>enquiries@asaff.com</p>
-        <p>673 2440959/245959</p>
+        <p>673 2440959/2454959</p>
     </section>
 )
 
